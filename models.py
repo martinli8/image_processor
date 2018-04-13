cc699a4c6418ebd90493eb47839ba008354fe692
David Brenes <db240@duke.edu>
2018-04-13 12:59:06 -0400

    Created User class with attributes

--- models.py
+++ models.py
@@ -2,5 +2,9 @@ from pymodm import fields, MongoModel
 
 
 class User(MongoModel):
+    email = fields.email(primary_key = True)
+    picture = fields.LineStringField()
+    process_duration = fields.ListFields()
+    upload_time = fields.ListFields()
+    image_size = fields.ListFields() # Tupel
     pass
-    
