76197dedb3dfa729596fd55a7abe6168fa2641ab
Martin Li (ml328) <martinli841@gmail.com>
2018-04-19 22:22:30 -0400

    updated Docstrings and unit tests for return_metadata and add_user_data

--- test_user.py
+++ test_user.py
@@ -26,3 +26,32 @@ def test_write_duration_time():
     create_user(testUser, picture, process_requested, timestamp, imageSize)
     user = models.User.objects.raw({"_id": "testUserEmail@email.com"}).first()
     assert write_duration_time(testUser, 6) == [6]
+
+
+def test_add_user_data():
+    create_user(testUser, picture, process_requested, timestamp, imageSize)
+    picture_append = "zyzz"
+    process_requested_append = "reverse_video"
+    timestamp_append = datetime.datetime(2018, 4, 20, 4, 20, 4, 847000)
+    imageSize_append = [512, 512]
+    add_user_data(testUser, picture_append, process_requested_append,
+                  timestamp_append, imageSize_append)
+    user = models.User.objects.raw({"_id": "testUserEmail@email.com"}).first()
+    assert user.email == testUser
+    assert user.picture == [picture, picture_append]
+    assert user.process_requested == ["histogram equalization", "reverse_video"]
+    assert user.upload_time == [timestamp, timestamp_append]
+    assert user.image_size == [[255, 255], [512, 512]]
+
+
+def test_return_metadata():
+    create_user(testUser, picture, process_requested, timestamp, imageSize)
+    assertDict = {
+        "user_email": testUser,
+        "pictures": [picture],
+        "process_requested": [process_requested],
+        "upload_time": [timestamp],
+        "image_size": [imageSize],
+        "process_duration": []
+    }
+    assert return_metadata(testUser) == assertDict
