faa544ea49d4dfeab67eadc96b74bfdaa56ccefe
ethanolnoknow <31057180+ethanolnoknow@users.noreply.github.com>
2018-04-30 00:41:38 -0400

    Combined Upload.js and SimpleSelect.js into ImageProcessor.js

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+Todo:
+Currently all the front end can do is send in the post requests. We still need to be able to display the processed image and obtain and display the histograms (both will require changes on the backend). Refer to my comments on line 30 in ImageProcessor.js
+
 To be able to run App.js, be sure to install by the following packages by typing:
 
 ```
--- image_processor_viewer/src/App.js
+++ image_processor_viewer/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import Upload from './Upload.js'
-import SimpleSelect from './SimpleSelect.js'
+// import Upload from './Upload.js'
+// import SimpleSelect from './SimpleSelect.js'
+import ImageProcessor from './ImageProcessor.js'
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,8 +13,7 @@ class App extends Component {
       <div>
 	  	  <MuiThemeProvider>
 			     <AppBar title="Image Processor" showMenuIconButton={false}/>
-			        <Upload />
-              <SimpleSelect />
+              <ImageProcessor />
         </MuiThemeProvider>
       </div>
     );
